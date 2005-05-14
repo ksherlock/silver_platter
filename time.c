3895c97e024ca4334c5b6a3da13250b6b9346837
Kelvin Sherlock <ksherlock@gmail.com>
2005-05-14 12:38:33 -0400

    fixed hh:mm:hh - s/b hh:mm:ss. Added function for ISO 8601.

--- time.c
+++ time.c
@@ -1,10 +1,62 @@
 #pragma noroot
 #pragma optimize -1
 #pragma lint -1
+#pragma debug 0x8000
 
 #include <timetool.h>
 #include <misctool.h>
 #include <intmath.h>
+
+
+// yyyy-mm-ddThh:mm:ssZ
+void tiTimeRec2ISO8601(const TimeRecPtr t, char *str)
+{
+LongWord secs;
+tiPrefRec tiPrefs;
+TimeRec tr;
+
+
+  tiPrefs.pCount = 3;
+  tiGetTimePrefs(&tiPrefs);
+
+  secs = ConvSeconds(TimeRec2Secs, 0, (Pointer)t);
+
+  secs += tiPrefs.secOffset;
+
+  ConvSeconds(secs2TimeRec, secs, (Pointer)&tr);
+
+  str[0] = 20;
+
+  // yyyy-
+  Int2Dec(tr.year + 1900, &str[1], 4, 0);
+  str[5] = '-';
+
+  // mm-
+  Int2Dec(tr.month + 1, &str[6], 2, 0);
+  str[6] |= 0x10; // convert ' ' -> '0'
+  str[8] = '-';
+
+  // ddT
+  Int2Dec(tr.day + 1, &str[9], 2, 0);
+  str[9] |= 0x10; // convert ' ' -> '0'
+  str[11] = 'T';
+
+  // hh:
+  Int2Dec(tr.hour, &str[12], 2, 0);
+  str[12] |= 0x10; // convert ' ' -> '0'
+  str[14] = ':';
+
+  // mm:
+  Int2Dec(tr.minute, &str[15], 2, 0);
+  str[15] |= 0x10; // convert ' ' -> '0'
+  str[17] = ':';
+
+  // ss:
+  Int2Dec(tr.second, &str[18], 2, 0);
+  str[18] |= 0x10; // convert ' ' -> '0'
+  str[20] = 'Z';
+
+}
 
 void tiTimeRec2GMTString(const TimeRecPtr t, char *str)
 {
@@ -44,7 +96,7 @@ TimeRec tr;
 
   // day
   Int2Dec(tr.day + 1, &str[6], 2, 0);
-  if (str[6] == ' ') str[6] = '0';
+  str[6] |= 0x10;
 
   str[8] = ' ';
 
@@ -59,15 +111,15 @@ TimeRec tr;
   str[17] = ' ';
 
   Int2Dec(tr.hour, &str[18], 2, 0);
-  if (str[18] == ' ') str[18] = '0';
+  str[18] |= 0x10;
   str[20] = ':';
 
   Int2Dec(tr.minute, &str[21], 2, 0);
-  if (str[21] == ' ') str[21] = '0';
+  str[21] |= 0x10;
   str[23] = ':';
 
-  Int2Dec(tr.hour, &str[24], 2, 0);
-  if (str[24] == ' ') str[24] = '0';
+  Int2Dec(tr.second, &str[24], 2, 0);
+  str[24] |= 0x10;
 
   str[26] = ' ';
   str[27] = 'G';
