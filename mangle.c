cf12ddac9b6e530f6224194a370e7770c3c608c7
Kelvin Sherlock <ksherlock@gmail.com>
2005-05-15 12:41:54 -0400

    Added function to convert MacRoman -> utf8.

--- mangle.c
+++ mangle.c
@@ -76,150 +76,151 @@ GSString255Ptr dest;
 }
 
 
-#define x(y) { sizeof(y) -1 , y }
+#define x(utf, y) { utf, sizeof(y) -1 , y}
 
 struct ConvTable
 {
+  Word utf;
   Word length;
   char *text;
 } mr2html[] =
 {
   //0x80
-  x("Auml"),
-  x("Aring"),
-  x("Ccedil"),
-  x("Eacute"),
-  x("Ntilde"),
-  x("Ouml"),
-  x("Uuml"),
-  x("aacute"),
-  x("agrave"),
-  x("acirc"),
-  x("auml"),
-  x("atilde"),
-  x("aring"),
-  x("ccedil"),
-  x("eacute"),
-  x("egrave"),
+  x(0xc4, "Auml"),
+  x(0xc5, "Aring"),
+  x(0xc7, "Ccedil"),
+  x(0xc9, "Eacute"),
+  x(0xd1, "Ntilde"),
+  x(0xd6, "Ouml"),
+  x(0xdc, "Uuml"),
+  x(0xe1, "aacute"),
+  x(0xe0, "agrave"),
+  x(0xe2, "acirc"),
+  x(0xe4, "auml"),
+  x(0xe3, "atilde"),
+  x(0xe5, "aring"),
+  x(0xe7, "ccedil"),
+  x(0xe9, "eacute"),
+  x(0xe8, "egrave"),
   //0x90
-  x("ecirc"),
-  x("euml"),
-  x("iacute"),
-  x("igrave"),
-  x("icirc"),
-  x("iuml"),
-  x("ntilde"),
-  x("oacute"),
-  x("ograve"),
-  x("ocirc"),
-  x("ouml"),
-  x("otilde"),
-  x("uacute"),
-  x("ugrave"),
-  x("ucirc"),
-  x("uuml"),
+  x(0xea, "ecirc"),
+  x(0xeb, "euml"),
+  x(0xed, "iacute"),
+  x(0xec, "igrave"),
+  x(0xee, "icirc"),
+  x(0xef, "iuml"),
+  x(0xf1, "ntilde"),
+  x(0xf3, "oacute"),
+  x(0xf2, "ograve"),
+  x(0xf4, "ocirc"),
+  x(0xf6, "ouml"),
+  x(0xf5, "otilde"),
+  x(0xfa, "uacute"),
+  x(0xf9, "ugrave"),
+  x(0xfb, "ucirc"),
+  x(0xfc, "uuml"),
   //0xA0      
-  x("dagger"),
-  x("deg"),
-  x("cent"),
-  x("pound"),
-  x("sect"),
-  x("bull"),
-  x("para"),
-  x("szlig"),
-  x("reg"),
-  x("copy"),
-  x("trade"),
-  x("acute"),
-  x("uml"),
-  x("ne"),
-  x("AElig"),
-  x("Oslash"),
+  x(0x2020, "dagger"),
+  x(0xb0, "deg"),
+  x(0xa2, "cent"),
+  x(0xa3, "pound"),
+  x(0xa7, "sect"),
+  x(0x2022, "bull"),
+  x(0xb6, "para"),
+  x(0xdf, "szlig"),
+  x(0xae, "reg"),
+  x(0xa9, "copy"),
+  x(0x2122, "trade"),
+  x(0xb4, "acute"),
+  x(0xa8, "uml"),
+  x(0x2260, "ne"),
+  x(0xc6, "AElig"),
+  x(0xd8, "Oslash"),
   //0xB0      
-  x("infin"),
-  x("plusmn"),
-  x("le"),
-  x("ge"),
-  x("yen"),
-  x("micro"),
-  x("part"),
-  x("sum"),
-  x("product"),
-  x("pi"),
-  x("int"),
-  x("ordf"),
-  x("ordm"),
-  x("Omega"),
-  x("aelig"),
-  x("oslash"),
+  x(0x221e, "infin"),
+  x(0xb1, "plusmn"),
+  x(0x2264, "le"),
+  x(0x2265, "ge"),
+  x(0xa5, "yen"),
+  x(0xb5, "micro"),
+  x(0x2202, "part"),
+  x(0x2211, "sum"),
+  x(0x220f, "product"),
+  x(0x03c0, "pi"),
+  x(0x222b, "int"),
+  x(0xaa, "ordf"),
+  x(0xba, "ordm"),
+  x(0x03a9, "Omega"),
+  x(0xe6, "aelig"),
+  x(0xf8, "oslash"),
   //0xC0
-  x("iquest"),
-  x("iexcl"),
-  x("not"),
-  x("radic"),
-  x("fnof"),
-  x("asymp"),
-  x("#x2206"),
-  x("laquo"),
-  x("raquo"),
-  x("hellip"),
-  x("nbsp"),
-  x("Agrave"),
-  x("Atilde"),
-  x("Otilde"),
-  x("OElig"),
-  x("oelig"),
+  x(0xbf, "iquest"),
+  x(0xa1, "iexcl"),
+  x(0xac, "not"),
+  x(0x221a, "radic"),
+  x(0x0192, "fnof"),
+  x(0x2248, "asymp"),
+  x(0x2206, "#x2206"),
+  x(0xab, "laquo"),
+  x(0xbb, "raquo"),
+  x(0x2026, "hellip"),
+  x(0xa0, "nbsp"),
+  x(0xc0, "Agrave"),
+  x(0xc3, "Atilde"),
+  x(0xd5, "Otilde"),
+  x(0x152, "OElig"),
+  x(0x153, "oelig"),
   // 0xD0
-  x("ndash"),
-  x("mdash"),    
-  x("ldquo"),    
-  x("rdquo"),    
-  x("lsquo"),    
-  x("rsquo"),    
-  x("divide"),    
-  x("loz"),    
-  x("yuml"),    
-  x("Yuml"),    
-  x("frasl"),    
-  x("euro"),    
-  x("lsaquo"),    
-  x("rsaquo"),    
-  x("#xfb01"),    
-  x("#xfb02"),
+  x(0x2013, "ndash"),
+  x(0x2014, "mdash"),    
+  x(0x201c, "ldquo"),    
+  x(0x201d, "rdquo"),    
+  x(0x2018, "lsquo"),    
+  x(0x2019, "rsquo"),    
+  x(0xf7, "divide"),    
+  x(0x25ca, "loz"),    
+  x(0xff, "yuml"),    
+  x(0x0178, "Yuml"),    
+  x(0x2044, "frasl"),    
+  x(0x20ac, "euro"),    
+  x(0x2039, "lsaquo"),    
+  x(0x203a, "rsaquo"),    
+  x(0xfb01, "#xfb01"),    
+  x(0xfb02, "#xfb02"),
   // 0xE0
-  x("Dagger"),
-  x("middot"),
-  x("sbquo"),
-  x("bdquo"),
-  x("permil"),
-  x("Acirc"),
-  x("Ecirc"),
-  x("Aacute"),
-  x("Euml"),
-  x("Egrave"),
-  x("Iacute"),
-  x("Icirc"),
-  x("Iuml"),
-  x("Igrave"),
-  x("Oacute"),
-  x("Ocirc"),
+  x(0x2021, "Dagger"),
+  x(0xb7, "middot"),
+  x(0x201a, "sbquo"),
+  x(0x201e, "bdquo"),
+  x(0x2030, "permil"),
+  x(0xc2, "Acirc"),
+  x(0xca, "Ecirc"),
+  x(0xc1, "Aacute"),
+  x(0xcb, "Euml"),
+  x(0xc8, "Egrave"),
+  x(0xcd, "Iacute"),
+  x(0xce, "Icirc"),
+  x(0xcf, "Iuml"),
+  x(0xcc, "Igrave"),
+  x(0xd3, "Oacute"),
+  x(0xd4, "Ocirc"),
   // 0xF0
-  x("#xf8ff"),  // apple logo
-  x("Ograve"),
-  x("Uacute"),
-  x("Ucirc"),
-  x("Ugrave"),
-  x("#x0131"),
-  x("circ"),
-  x("tilde"),
-  x("macr"),
-  x("#x02d8"),
-  x("#x02d9"),
-  x("#x02da"),
-  x("cedil"),
-  x("#x02dd"),
-  x("#x02db"),
-  x("#x02c7")
+  x(0xf8ff, "#xf8ff"),  // apple logo
+  x(0xd2, "Ograve"),
+  x(0xda, "Uacute"),
+  x(0xdb, "Ucirc"),
+  x(0xd9, "Ugrave"),
+  x(0x0131, "#x0131"),
+  x(0x02c6, "circ"),
+  x(0x02dc, "tilde"),
+  x(0xaf, "macr"),
+  x(0x02d8, "#x02d8"),
+  x(0x02d9, "#x02d9"),
+  x(0x2da, "#x02da"),
+  x(0xb8, "cedil"),
+  x(0x02dd, "#x02dd"),
+  x(0x02db, "#x02db"),
+  x(0x02c7, "#x02c7")
 };        
 #undef x
 
@@ -325,3 +326,65 @@ GSString255Ptr dest;
 
   return h;
 }
+
+
+// Convert MacRoman --> utf-8
+// returns:
+//-1 on error
+//0 if no conversion needed
+// valid handle otherwise.
+Handle MacRoman2UTF8(const GSString255 *gstr)
+{
+Word i, j;
+char c;
+Word extra;
+Handle h;
+GSString255Ptr dest;
+Word utf;
+
+  extra = 0;
+  for (i = 0; i < gstr->length; i++)
+  {
+    c = gstr->text[i];
+    if (c & 0x80)
+    {
+    	utf = mr2html[c & 0x7f].utf;
+    	if (utf > 0x0fff) extra += 3; //
+    	else extra += 2;
+    }
+  }
+  if (!extra) return NULL;
+
+  h = NewHandle(gstr->length + 3 + extra, MyID | 0x0e00, attrLocked, NULL);
+  if (_toolErr) return (Handle)0; //-1;
+
+  dest = (GSString255Ptr)*h;
+
+  i = j = 0;
+  while ( i < gstr->length)
+  {
+    c = gstr->text[i++];
+    if (c & 0x80)
+    {
+    	utf = mr2html[c & 0x7f].utf;
+    	if (utf > 0x0fff)
+    	{
+    	  dest->text[j++] = 0xe0 | (utf >> 12);
+    	  dest->text[j++] = 0x80 | ((utf >> 6) & 0x3f);
+    	  dest->text[j++] = 0x80 | (utf & 0x3f);	
+    	}
+    	else
+    	{
+    		dest->text[j++] = 0xc0 | (utf >> 6);
+    		dest->text[j++] = 0x80 | (utf & 0x3f);
+    	}
+    }
+    else dest->text[j++] = c;
+  }
+  dest->text[j] = 0; // make a cstring
+  dest->length = j;
+
+  return h;
+}
+
+
